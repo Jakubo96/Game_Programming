4a50ab4b28bc0fe9c7a0c8f9de6453a9b0f43335
Jakub Woźny <jakubovip@gmail.com>
2019-09-07 18:55:56 +0200

    Handle player movement

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -1,18 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+//    private PlayerAnimation playerAnimation;
+    private Rigidbody myBody;
+
+    public float walkSpeed = 2f;
+    public float zSpeed = 1.5f;
+
+    private float rotationY = -90f;
+    private float rotationSpeed = 15f;
+
+    void Awake()
     {
-        
+//        playerAnimation = GetComponentInChildren < PlayerAnimation();
+        myBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        RotatePlayer();
+    }
+
+    private void FixedUpdate()
+    {
+        DetectMovement();
+    }
+
+    void DetectMovement()
+    {
+        myBody.velocity = new Vector3(
+            Input.GetAxisRaw(Axis.HORIZONTAL_AXIS) * (-walkSpeed),
+            myBody.velocity.y,
+            Input.GetAxisRaw(Axis.VERTICAL_AXIS) * (-zSpeed)
+        );
+    }
+
+    void RotatePlayer()
+    {
+        if(Input.GetAxisRaw(Axis.HORIZONTAL_AXIS) > 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, -Mathf.Abs(rotationY), 0f);
+        } else if (Input.GetAxisRaw(Axis.HORIZONTAL_AXIS) < 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, Mathf.Abs(rotationY), 0f);
+        }
     }
 }
