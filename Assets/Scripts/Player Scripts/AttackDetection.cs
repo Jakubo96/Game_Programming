73c4d646b74821861ad3e2c34b4c277e7dcb9789
Jakub Woźny <jakubovip@gmail.com>
2019-09-08 16:57:18 +0200

    Handle collision (hit point) detection

---
+++ Assets/Scripts/Player Scripts/AttackDetection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class AttackDetection : MonoBehaviour
+{
+    public LayerMask collisionLayer;
+    public float radius = 1f;
+    public float damage = 2f;
+
+    public bool isPlayer;
+    public bool isEnemy;
+
+    public GameObject hitFx;
+
+    private void Update()
+    {
+        DetectCollision();
+    }
+
+    void DetectCollision()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, radius, collisionLayer);
+        
+        if(hits.Length > 0)
+        {
+        }
+    }
+}
